7dc5027263d5c0a9c5a6efa6e33686d921d72d55
estefanozaratem <114966707+estefanozarate@users.noreply.github.com>
2023-11-08 17:23:57 -0500

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,14 @@
+insert into person(name) values('Juan Carlos Peralta');
+insert into person(name) values('Sergio Caceres');
+insert into person(name) values('Jorge Zulman');
+insert into person(name) values('Rodrigo Alarcon');
+insert into person(name) values('Dayana Palomino');
+
+
+insert into groups(name) values('Amigos');
+insert into groups(name) values('Trabajo');
+insert into groups(name) values('Universidad');
+
+insert into person_group(group_id,person_id) values(1 , 1);
+insert into person_group(group_id,person_id) values(1 , 2);
+insert into person_group(group_id,person_id) values(2 , 1);
